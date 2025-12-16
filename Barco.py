13dbc1d4a159c479e97912798ae9bbdad675af55
Victor <vpenavazquez@danielcastelao.org>
2025-12-16 22:32:10 +0100

    Barco U2: Optimización de armamento y alcance

--- Barco.py
+++ Barco.py
@@ -2,3 +2,15 @@ class Barco:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
+    def getwidth(self):
+        return self.width
+    def getheight(self):
+        return self.height
+
+    def setwidth(self, width):
+        self.width = width
+    def setheight(self, height):
+        self.height = height
+    def __str__(self):
+        return f'{self.width}x{self.height}'
