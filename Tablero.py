990a123a41f77ee5ffd9f60d609110aff06aa1ed
Victor <vpenavazquez@danielcastelao.org>
2025-12-16 22:13:46 +0100

    "Tablero U2: Implementación de la cuadrícula de ataque."

---
+++ Tablero.py
@@ -0,0 +1,10 @@
+class tablero:
+    def __init__(self, tablero):
+        self.tablero = tablero
+
+
+    def __str__(self):
+        return self.__String__()
+
+if __name__ == "__main__":
+    tablero = tablero(tablero)
